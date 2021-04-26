7ca664467bcfe428c3208ef1430146d631cff5b0
Trey K <80800860+treykapfer@users.noreply.github.com>
2021-04-26 14:57:31 -0500

    tavern_update

---
+++ static/js/hidden_menus.js
@@ -0,0 +1,42 @@
+function open_inventory() {
+    inv_menu = document.getElementById("inventory_menu");
+    inv_menu.style.removeProperty("display");
+    inv_menu.style.display = "block";
+}
+
+function close_inventory() {
+    inv_menu = document.getElementById("inventory_menu");
+    inv_menu.style.removeProperty("display");
+    inv_menu.style.display = "none";
+}
+
+function open_shop() {
+    tav_shop = document.getElementById("tavern_shop");
+    tav_feed = document.getElementById("tavern_activity_feed");
+    tav_parent = document.getElementById("tavern_lower_box")
+
+    tav_feed.style.removeProperty("display");
+    tav_feed.style.display = "none";
+
+    tav_shop.style.removeProperty("display");
+    tav_shop.style.display = "flex";
+
+    tav_parent.style.justifyContent = "center";
+    tav_parent.style.alignItems = "center";
+}
+
+function close_shop() {
+    tav_shop = document.getElementById("tavern_shop");
+    tav_feed = document.getElementById("tavern_activity_feed");
+    tav_parent = document.getElementById("tavern_lower_box")
+
+    tav_shop.style.removeProperty("display");
+    tav_shop.style.display = "none";
+
+    tav_feed.style.removeProperty("display");
+    tav_feed.style.display = "flex";
+
+    tav_parent.style.removeProperty("justifyContent");
+    tav_parent.style.removeProperty("alignItems");
+    tav_parent.style.justifyContent = "flex-start";
+}
